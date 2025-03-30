c11075670bb85e271ca635880035eb04e1fe13ab
Bohdan <dennger228@gmail.com>
2025-03-30 11:23:38 +0300

    lab 4 - added role based authorization to spring api app

--- src/main/java/edu/pzks/opensource/config/SecurityConfig.java
+++ src/main/java/edu/pzks/opensource/config/SecurityConfig.java
@@ -1,17 +1,19 @@
 package edu.pzks.opensource.config;
 
+import edu.pzks.opensource.jwt.KeycloakJwtAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
   @Bean
@@ -26,7 +28,7 @@ public class SecurityConfig {
     http
             .oauth2ResourceServer(
                     auth -> auth.jwt(
-                            jwt -> jwt.jwtAuthenticationConverter(new JwtAuthenticationConverter()))
+                            jwt -> jwt.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
             );
 
     http.
